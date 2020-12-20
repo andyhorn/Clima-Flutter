2e59b063d13804e6852fb52ee4b1f5c294c7cf80
Andy <andyjhorn@gmail.com>
2020-12-20 17:27:12 -0600

    minor cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:clima/screens/loading_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-// void main() => runApp(MyApp());
 Future main() async {
   await DotEnv().load('.env');
   runApp(MyApp());
@@ -11,7 +10,6 @@ Future main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       theme: ThemeData.dark(),
       home: LoadingScreen(),
